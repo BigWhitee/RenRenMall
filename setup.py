972bd5294a8e0d8175cb22f6df9b4170a0144538
BigWhitee <43695412+BigWhitee@users.noreply.github.com>
2022-04-04 17:21:56 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup
 
 setup(
     name='renren_mall',  # 应用名
-    version='0.0.1',  # 版本号
+    version='0.0.2',  # 版本号
     packages=['api', 'common', 'configs', 'factory', 'MyLog'],  # 包括在安装包内的 Python 包
 )
