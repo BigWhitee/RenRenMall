256d7ef8e91c4bef74f6c4bc8188a198b6cc70d3
BigWhitee <43695412+BigWhitee@users.noreply.github.com>
2022-04-04 18:22:04 +0800

    优化

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='RenrenMall',  # 应用名
+    name='renren_mall_manage',  # 应用名
     version='0.0.4',  # 版本号
     author='YarnBlue',
     packages=find_packages(),
