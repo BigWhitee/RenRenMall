a360fc5a99c80c2d4abd39649128e4422b06476c
BigWhitee <43695412+BigWhitee@users.noreply.github.com>
2022-04-04 16:41:59 +0800

    优化

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name='renren_mall',  # 应用名
     version='0.0.1',  # 版本号
-    packages=['renren_mall'],  # 包括在安装包内的 Python 包
+    packages=['api', 'common', 'configs', 'factory', 'MyLog'],  # 包括在安装包内的 Python 包
 )
