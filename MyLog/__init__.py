54a1177d6c5050be7f41e1b75a566793ddeafa45
BigWhitee <zqb090325@gmail.com>
2022-04-05 17:57:54 +0800

    优化

--- MyLog/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-@Time : 2022/4/4 16:44 
-@Author : YarnBlue 
-@description : 
-@File : __init__.py
-"""
--- setup.py
+++ setup.py
@@ -2,15 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='RenRenMall',  # 应用名
-    version='0.0.8',  # 版本号
+    version='0.0.9',  # 版本号
     author='YarnBlue',
     author_email='zqb090325@126.com',
     description='人人商城后台管理',
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
     packages=find_packages(),
     include_package_data=True
 )
