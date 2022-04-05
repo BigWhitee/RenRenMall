817805be25071910a16e68be6b4d3f99becad575
BigWhitee <zqb090325@gmail.com>
2022-04-05 18:15:27 +0800

    优化

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RenRenMall',  # 应用名
-    version='0.0.9',  # 版本号
+    version='0.0.10',  # 版本号
     author='YarnBlue',
     author_email='zqb090325@126.com',
     description='人人商城后台管理',
