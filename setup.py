a095cbf1854a1ce564eb16ab6dd18b7966145b54
BigWhitee <zqb090325@gmail.com>
2022-04-05 16:00:27 +0800

    优化

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='renren_mall_manage',  # 应用名
-    version='0.0.5',  # 版本号
+    name='RenRenMall',  # 应用名
+    version='0.0.6',  # 版本号
     author='YarnBlue',
-    packages=find_packages(),   
+    author_email='zqb090325@126.com',
+    description='人人商城后台管理',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(),
+    include_package_data=True
+
 )
