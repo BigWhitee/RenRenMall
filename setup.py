736da82e35cfab1c94a8abf3bb71d065a5baefac
BigWhitee <43695412+BigWhitee@users.noreply.github.com>
2022-04-06 10:25:59 +0800

    优化

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name='RenRen_Shop',  # 应用名
-    version='0.1.1',  # 版本号
+    version='0.1.2',  # 版本号
     author='YarnBlue',
     author_email='zqb090325@126.com',
     description='人人商城后台管理',
-    packages=['renren_mall'],
+    packages=['RenRen_Shop'],
     include_package_data=True
 )
