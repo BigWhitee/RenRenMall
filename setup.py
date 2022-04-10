638ad4b9307286df61d06c28e29025f018c8fdcf
BigWhitee <43695412+BigWhitee@users.noreply.github.com>
2022-04-10 14:38:46 +0800

    版本号

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RenRen_Shop',  # 应用名
-    version='1.0.2',  # 版本号
+    version='1.0.3',  # 版本号
     author='YarnBlue',
     author_email='zqb090325@126.com',
     description='人人商城后台管理',
