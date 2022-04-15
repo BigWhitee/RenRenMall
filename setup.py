d045a5e14ca9d6e81b3b2003d9d7e341e654e5fe
BigWhitee <43695412+BigWhitee@users.noreply.github.com>
2022-04-15 16:56:53 +0800

    新版本更新

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='RenRen_Shop',  # 应用名
-    version='1.0.5',  # 版本号
+    version='1.0.6',  # 版本号
     author='YarnBlue',
     author_email='zqb090325@126.com',
     description='人人商城后台管理',
