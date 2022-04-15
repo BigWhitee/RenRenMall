356957727b0349f228495482c12a8e7347cd300f
BigWhitee <43695412+BigWhitee@users.noreply.github.com>
2022-04-15 16:55:07 +0800

    测试文件

--- test/test.py
+++ test/test.py
@@ -11,7 +11,8 @@ import time
 
 from RenRen_Shop.factory import Factory
 
-def main(client):
+
+def main(client: Factory):
     Fetcher = client.seckill.FetchSecKillList
     Fetcher.next(keyword='整点秒杀')
     seckill_list = Fetcher.result()
@@ -38,25 +39,28 @@ def seckill(client):
     }
     client.app.MySecKill.add_mySecKill_with_filter_and_time(start_dt, **data)
 
-def daily_seckill(client):
+def daily_seckill(client: Factory):
     """
     随机获取1/7的商品，加入每日特价组，按大促的价格，并编辑主页内容，实现每日特价功能
     :param client:
     :return:
     """
+    goodsIds = client.goods.filter_goods(group='3858')
+
     goods_ids = client.goods.filter_goods(key='category__0__category_id', value=3396, judge='!=')
     goods_count = len(goods_ids)
     goods_ids = random.sample(goods_ids, int(round(goods_count / 7)))
-    GroupFetcher = client.group.FetchGroupsList
+    if client.category.set_category(0, [3858], *goods_ids):
+        client.logger.info(f'{len(goods_ids)}个商品成功加入每日特价分类')
     if client.group.update_groups('每日特价', *goods_ids):
-        client.logger.info(f'{len(goods_ids)}个商品成功加入每日特价')
+        client.logger.info(f'{len(goods_ids)}个商品成功加入每日特价分组')
 
     start_time = '2022-04-14 00:00:00'
     duration = 24 * 3600
     if client.seckill.seckill_add_quick(*goods_ids, start_time=start_time, duration=duration, title='每日特价'):
         client.logger.info('每日特价秒杀活动新建成功')
 
+
 if __name__ == '__main__':
     with Factory() as client:
         pass
-
