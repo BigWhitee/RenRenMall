c9126a084989673280d8b93df27ed013f1e17632
BigWhitee <43695412+BigWhitee@users.noreply.github.com>
2022-04-15 09:09:47 +0800

    优化注释

--- RenRen_Shop/api/application/diypage/page_infos.py
+++ RenRen_Shop/api/application/diypage/page_infos.py
@@ -8,11 +8,36 @@
 import json
 
 from RenRen_Shop.api.RenRen_api import RenRenApi
+from RenRen_Shop.common import logger
 
 
 class PageInfos(RenRenApi):
-    def page_infos(self, id, type=10) -> json:
+    def page_infos(self, id, type) -> json:
+        """
+        返回格式为：
+        ========================
+        {
+            'common': json.dumps(dict),
+            'content': json.dumps(list),
+            'creat_time':str,
+            'id': str,
+            'shop_id': str,
+            'status': str,
+            'template_id': str,
+            'thumb': str,
+            'type': str,
+            'update_time': str
+        }
+        ========================
+        :param id:
+        :param type:
+        :return:
+        """
         rep = self.session.get(self.URL.page_edit(),
                                params={'id': id, 'type': type},
                                **self.kwargs)
-        return rep.json()
+        if rep.json()['error'] == 0:
+            return rep.json()['data']
+        else:
+            logger.error(rep.text)
+            return False
